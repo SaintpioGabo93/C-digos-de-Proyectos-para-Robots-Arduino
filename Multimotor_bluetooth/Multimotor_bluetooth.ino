bdd5b032986f2c78c2225d8727d70754c69f79b1
Gabo <121601624+SaintpioGabo93@users.noreply.github.com>
2023-06-15 20:43:46 -0600

    Update Multimotor_bluetooth.ino

--- Multimotor_bluetooth/Multimotor_bluetooth.ino
+++ Multimotor_bluetooth/Multimotor_bluetooth.ino
@@ -19,12 +19,14 @@ Servo servo2;
 // Objeto motor dc
 
 AF_DCMotor motor1(1,MOTOR12_2KHZ);
+AF_DCMotor motor2(4,MOTOR12_2KHZ);
 
 void setup() {
   Serial.begin(9600);
   servo1.attach(10,600,2400);
   servo2.attach(9,600,2400);
-  motor1.run(RELEASE);
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
   
 }
 
@@ -32,18 +34,24 @@ void loop() {
   while(Serial.available() > 0) 
   {
     val = Serial.read();
-
+    }
  
   if (val == 'd')
   {
     angulo1 = angulo1 + 5; //Se aumenta el angulo en 5 grados
     Serial.print(angulo1); // Se envía el valor puerto serial
+    angulo1 = constrain(angulo1,0,180);
+    servo1.write(angulo1);
+    delay(100);
     }
 
   else if (val == 'i')
   {
     angulo1 = angulo1 - 5; //Se aumenta el angulo en 5 grados
-    Serial.print(angulo1); // Se envía el valor puerto serial     
+    Serial.print(angulo1); // Se envía el valor puerto serial
+    angulo1 = constrain(angulo1,0,180);
+    servo1.write(angulo1);
+    delay(100);     
     }
 
 
@@ -51,34 +59,40 @@ void loop() {
   {
     angulo2 = angulo2 + 5; //Se aumenta el angulo en 5 grados
     Serial.print(angulo2); // Se envía el valor puerto serial
+    angulo2 = constrain(angulo2,0,180);
+    servo2.write(angulo2);
+    delay(100);      
     }
 
   else if (val == 'f')
   {
     angulo2 = angulo2 - 5; //Se aumenta el angulo en 5 grados
     Serial.print(angulo2); // Se envía el valor puerto serial
+    angulo2 = constrain(angulo2,0,180);
+    servo2.write(angulo2);
+    delay(100);     
     }            
-  angulo1 = constrain(angulo1,0,180);
-  angulo2 = constrain(angulo2,0,180);
-  }
 
-
-  if( val == 'l')
+  else if( val == 'l')
   {
     motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    Serial.println(val);
     } 
-
-
+    
   else if( val == 'n')
   {
     motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    Serial.println(val);
     }  
-  
 
-  servo1.write(angulo1);
-  delay(100);
+ else if (val == "z")
+ {
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  Serial.print(val);
+  }
 
-
-  servo2.write(angulo2);
-  delay(100);  
+ 
 }
